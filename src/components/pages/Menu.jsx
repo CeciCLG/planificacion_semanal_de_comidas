32755cc932d8fb7047aa423ae80f8930af13e782
Cecilia Christina <cecilia.lares@bootcamp-upgrade.com>
2024-07-07 11:21:49 +0200

    new branch context menu

--- src/components/pages/Menu.jsx
+++ src/components/pages/Menu.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom"
 
 function Menu() {
     return (
-        <section>
+        <>
             <h1>menu</h1>
             <Link to="/detail">Detail</Link>
-        </section >
+        </ >
     )
 }
 
