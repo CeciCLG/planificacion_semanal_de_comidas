9af556651e2937b45571aad2b1dfadfcd6f35428
Cecilia Christina <cecilia.lares@bootcamp-upgrade.com>
2024-07-11 23:54:34 +0200

    creado el formulario SelectWeekDay

--- src/components/pages/Filtered.jsx
+++ src/components/pages/Filtered.jsx
@@ -1,22 +1,63 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { foodContext } from "../ContextFood/ContextFood";
+import SelectWeekDay from "../selectWeekDay/SelectWeekDay";
 
 
 function Filtered() {
     const context = useContext(foodContext);
 
-    const handleFilter = () => {
-        const dayWeek = "Lunes";
+    const initial_state = {
+        lunes: [],
+        martes: [],
+        miercoles: [],
+        jueves: [],
+        viernes: [],
+        sabado: [],
+        domingo: []
+    }
+
+    const [filteredWeek, setFilteredWeek] = useState(initial_state)
+
+
+    const [dayWeek, setDayWeek] = useState("");
+
+    const handleFilter = (ev) => {
+
+        ev.preventDefault()
+
         const arrayFood = context.menu;
 
-        context.filtered(arrayFood, dayWeek)
+        const filteredArray = context.getFilteredWeek(arrayFood, dayWeek);
 
+        if (dayWeek === "lunes") {
+            setFilteredWeek({ ...filteredWeek, lunes: filteredArray });
+
+        } else if (dayWeek === "martes") {
+            setFilteredWeek({ ...filteredWeek, martes: filteredArray });
+
+        } else if (dayWeek === "miercoles") {
+            setFilteredWeek({ ...filteredWeek, miercoles: filteredArray });
+
+        } else if (dayWeek === "jueves") {
+            setFilteredWeek({ ...filteredWeek, jueves: filteredArray });
+
+        } else if (dayWeek === "viernes") {
+            setFilteredWeek({ ...filteredWeek, viernes: filteredArray });
+
+        } else if (dayWeek === "sabado") {
+            setFilteredWeek({ ...filteredWeek, sabado: filteredArray });
+
+        } else if (dayWeek === "domingos") {
+            setFilteredWeek({ ...filteredWeek, domingos: filteredArray });
+
+        }
     };
 
     return (
-        <div>
+        <form>
+            <SelectWeekDay setSelectFunction={setDayWeek} />
             <button onClick={handleFilter}>Filtrar</button>
-        </div>
+        </form>
     )
 }
 
---
+++ src/components/selectWeekDay/SelectWeekDay.jsx
@@ -0,0 +1,26 @@
+
+
+function SelectWeekDay({ setSelectFunction }) {
+
+    const handleCollect = (ev) => {
+        ev.preventDefault()
+        console.log(ev.target.value);
+        setSelectFunction(ev.target.value);
+    }
+
+    return (
+        <form action="" onChange={handleCollect}>
+            <select name="dayWeek" id="dayWeek" >
+                <option value="lunes">Lunes</option>
+                <option value="martes">Martes</option>
+                <option value="miercoles">Miercoles</option>
+                <option value="jueves">Jueves</option>
+                <option value="viernes">Viernes</option>
+                <option value="sabado">Sabado</option>
+                <option value="domingo">Domingo</option>
+            </select>
+        </form>
+    )
+}
+
+export default SelectWeekDay
