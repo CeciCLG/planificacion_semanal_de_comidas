e95ca944a17a51c55826d1c2936cfa7216cf2c29
Cecilia <cecilia.lares@bootcamp-upgrade.com>
2024-07-17 00:15:35 +0200

    se cambia el orden de los links de la cabecera

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,9 +4,9 @@ function Header() {
     return (
         <header>
             <ul>
-                <li><Link to="/">Mi perfil</Link></li>
                 <li><Link to="/menu">Menu</Link></li>
                 <li><Link to="/filter">Filtros</Link></li>
+                <li><Link to="/">Mi perfil</Link></li>
             </ul>
         </header>
     )
