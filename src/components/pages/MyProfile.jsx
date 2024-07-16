9982984601320eae739160287f5fe1d16014f7c5
Cecilia <cecilia.lares@bootcamp-upgrade.com>
2024-07-17 00:13:55 +0200

    se elimina la opción de eliminar cuenta de myProfile

--- src/components/pages/MyProfile.jsx
+++ src/components/pages/MyProfile.jsx
@@ -1,25 +1,22 @@
-import UseForceUpdate from "../forceUpdate/ForceUpdate";
-import { getRemoveUser } from "../service/ApiUser";
-
 
 function MyProfile({ userData, setUserData }) {
     async function handleRemove() {
         await JSON.parse(localStorage.removeItem("user"));
     }
 
-    async function handleDelete(ev) {
+    /* async function handleDelete(ev) {
         ev.preventDefault();
         await getRemoveUser(userData);
         await setUserData(null);
         await JSON.parse(localStorage.removeItem("user"));;
         UseForceUpdate();
-    }
+    } */
 
     return (
         <form>
             <button onClick={handleRemove}>Cerrar sesión</button>
 
-            <button onClick={handleDelete}>Eliminar cuenta</button>
+            {/* <button onClick={handleDelete}>Eliminar cuenta</button> */}
         </form>
     )
 }
