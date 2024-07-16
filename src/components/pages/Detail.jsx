67db49f58e76ed82139b55fb086561b96d808ab2
Cecilia <cecilia.lares@bootcamp-upgrade.com>
2024-07-16 22:33:40 +0200

    se randeriza el componente clicado

--- src/components/pages/Detail.jsx
+++ src/components/pages/Detail.jsx
@@ -1,8 +1,34 @@
+import { useContext } from "react"
+import { useParams } from "react-router-dom"
+import { foodContext } from "../ContextFood/ContextFood";
+
 function Detail() {
+
+    const context = useContext(foodContext);
+
+    const params = useParams();
+
+    const item = context.menu.filter((food) => {
+        return food.id === params.id
+    })
+
+    const htmlItem = item.map((food) => (
+        <section key={food.id}>
+            <h1>{food.name}</h1>
+            <figure>
+                <img src={food.picture} alt={food.name} />
+                <figcaption>
+                    <h4>{food.type}</h4>
+                    <p>{food.descripcion}</p>
+                </figcaption>
+            </figure>
+        </section>
+    ))
+
     return (
-        <div>
-            <h1>Detail</h1>
-        </div>
+        <>
+            {htmlItem}
+        </>
     )
 }
 
